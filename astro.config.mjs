375701a466f27a4b8fe8c77400093c40004e935a
bikkyue <121682296+bikkyue@users.noreply.github.com>
2026-04-29 10:49:55 +0000

    remove: cloudflarePagesへ移行

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://github.com/bikkyue/bikkyue.github.io'
+  site: 'https://link.bikyu.dev'
 });
